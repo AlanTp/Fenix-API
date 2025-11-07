315b7392ff6ae0c4d2f7ea4953a802c2889c151c
Alan Meinberg <alantp2013@gmail.com>
2025-11-07 14:09:28 -0300

    tela de promotor, ajuste tela de cadatro batidas

--- server.js
+++ server.js
@@ -181,6 +181,74 @@ app.post("/Valvulas", async (req, res) => {
     }
 });
 
+//promotor
+
+app.get("/Promotor", async (req, res) => {
+    try {
+        const { colaborador, inicio, fim } = req.query;
+        let query = `SELECT 
+         data,
+         colaborador,
+         promotor_normal,
+         promotor_extra
+        FROM promotor WHERE 1=1`;
+        const values = [];
+        let count = 1;
+
+        if (colaborador) {
+            query += ` AND colaborador ILIKE $${count}`;
+            values.push(`%${colaborador}%`);
+            count++;
+        }
+
+        if (inicio && fim) {
+            query += ` AND data BETWEEN $${count} AND $${count + 1}`;
+            values.push(inicio, fim);
+            count += 2;
+        } else if (inicio) {
+            query += ` AND data >= $${count}`;
+            values.push(inicio);
+            count++;
+        } else if (fim) {
+            query += ` AND data <= $${count}`;
+            values.push(fim);
+            count++;
+        }
+
+        query += ` ORDER BY data DESC`;
+        const result = await pool.query(query, values);
+
+        res.json(result.rows);
+    } catch (e) {
+        console.error("Erro ao consultar o banco:", e);
+        res.status(500).send("Erro no servidor");
+    }
+});
+
+app.post("/Promotor", async (req, res) => {
+    try {
+        const { data, colaborador, promotor_normal, promotor_extra, user_name } = req.body;
+
+        const query = `
+            INSERT INTO promotor (data, colaborador, promotor_normal, promotor_extra, user_name)
+            VALUES ($1, $2, $3, $4, $5)
+                RETURNING *;
+        `;
+
+        const values = [data, colaborador, promotor_normal, promotor_extra, user_name];
+        const result = await pool.query(query, values);
+
+        res.status(201).json({
+            message: "✅ Batida Promotor inserida com sucesso!",
+            batida: result.rows[0],
+        });
+    } catch (error) {
+        console.error("❌ Erro ao inserir batidas Promotor:", error);
+        res.status(500).json({ error: "Erro no servidor" });
+    }
+});
+
+
 app.listen(PORT, () => {
     console.log(`🚀 API rodando em http://localhost:${PORT}`);
 });
