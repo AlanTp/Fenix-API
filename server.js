d2c91bf419861502d4aec365946ca83ce3084c99
Alan Meinberg <alantp2013@gmail.com>
2026-03-18 14:39:51 -0300

    Ajuste conexao

--- server.js
+++ server.js
@@ -44,7 +44,9 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
     port: Number(process.env.DB_PORT) || 5432,
-    ssl: { rejectUnauthorized: false },
+    ssl: {
+        require: true,
+        rejectUnauthorized: false },
 });
 
 // Rotas
